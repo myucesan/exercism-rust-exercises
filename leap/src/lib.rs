d681a6542e9bd5975fafdd886bda9d32f176f793
Mustafa Yücesan <my96@live.nl>
2021-10-12 21:13:06 +0200

    finished leap year first version

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,13 +1,15 @@
+
+/*
+on every year that is evenly divisible by 4
+  except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+*/
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 100 == 0 && year % 400 == 0 {
-        true;
-    } else if year % 100 == 0 {
-        false;
+    if year % 4 == 0 && !(year % 100 == 0) {
+        true
+    } else if year % 4 == 0 && year % 100 == 0 && year % 400 == 0 {
+        true
     } else {
-        true;
+        false
     }
-
-
-
-    false
 }
